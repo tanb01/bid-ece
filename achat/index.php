aab01607abb85ff950ed1f274639a6936defde97
tanb01 <tanb01@users.noreply.github.com>
2020-04-17 20:18:11 +0200

    Ajout de Navbar et de Footer

--- achat/index.php
+++ achat/index.php
@@ -48,7 +48,26 @@
 
     </div>
   </div>
-
+  
+  <!--Footer-->
+  <footer>
+    <div class="container">
+      <div class="my-auto text-white text-center py-4">
+        <h6 class="my-auto no-deco">2020
+          &copy; BID ECE<span></span></h6>
+        <div class="row">
+          BID ECE a été créé en 2020 pour permettre à chacun d’acheter et de vendre
+          les plus belles pièces uniques. Les prix affichés sont fixés par ces
+          vendeurs et BID ECE opère en tant qu’intermédiaire et tiers de confiance
+          auprès d’eux et des acheteurs. Ces derniers peuvent ainsi dénicher parmi
+          les 100 000 références de BID ECE la perle rare et être livrés sans
+          bouger de leur canapé. Les pièces proposées à la vente sont quant à
+          elles quotidiennement sélectionnées à la main par nos équipes.
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!--/.Footer-->
 
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
